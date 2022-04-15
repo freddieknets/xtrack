257724103e7f7c4dabe6a5d1efcfebdf45665f3f
giadarol <giovanni.iadarola@cern.ch>
2022-04-15 20:15:58 +0200

    Manage to call track kernel

--- examples/executable/executable_with_xobjects.c
+++ examples/executable/executable_with_xobjects.c
@@ -27,24 +27,30 @@ int main(){
     // Get sim config
     SimConfig sim_config = (SimConfig) sim_buffer;
 
-    printf("num_turns: %d\n", (int) SimConfig_get_num_turns(sim_config));
+    const int64_t num_turns = SimConfig_get_num_turns(sim_config);
+    const int64_t num_elements = SimConfig_len_line_metadata_ele_offsets(sim_config);
 
-    /*
+    printf("num_turns: %d\n", (int) num_turns);
+    printf("num_elements: %d\n", (int) num_elements);
+
+    int64_t* line_ele_offsets = SimConfig_getp1_line_metadata_ele_offsets(sim_config, 0);
+    int64_t* line_ele_typeids = SimConfig_getp1_line_metadata_ele_typeids(sim_config, 0);
+    ParticlesData particles = SimConfig_getp_particles(sim_config);
+
     // This is what we want to call
     track_line(
-          line_buffer, //    int8_t* buffer,
+          sim_buffer, //    int8_t* buffer,
           line_ele_offsets, //    int64_t* ele_offsets,
           line_ele_typeids, //    int64_t* ele_typeids,
-          part, //    ParticlesData particles,
-          2, //    int num_turns,
+          particles, //    ParticlesData particles,
+          (int) num_turns, //    int num_turns,
           0, //    int ele_start,
-          num_elements, //    int num_ele_track,
+          (int) num_elements, //    int num_ele_track,
           0, //int flag_end_turn_actions,
           0, //int flag_reset_s_at_end_turn,
           0, //    int flag_monitor,
           NULL,//    int8_t* buffer_tbt_monitor,
           0//    int64_t offset_tbt_monitor
     );
-    */
 
 }
