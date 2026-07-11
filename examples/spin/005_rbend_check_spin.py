4ebf802b5d67a012d67f3f0405177b25772c1ca3
giadarol <giovanni.iadarola@cern.ch>
2026-07-11 08:37:39 +0200

    In dipole wedge rotate spin vector (reference frame change)

--- examples/spin/005_rbend_check_spin.py
+++ examples/spin/005_rbend_check_spin.py
@@ -3,8 +3,11 @@ import numpy as np
 env = xt.Environment()
 env.new('rb', 'RBend', length_straight=2., angle=0.1)
 
+anomalous_magnetic_moment = 1.15965218091e-3
+
 line = env.new_line(components=['rb'])
-line.set_particle_ref('electron', energy0=5e9)
+line.set_particle_ref('electron', energy0=5e9,
+                      anomalous_magnetic_moment=anomalous_magnetic_moment)
 line.configure_bend_model(edge='full')
 
 two = line.twiss4d(betx=1, bety=1, spin=True, spin_x=1)
